6ee0f8d6581553edb6bae3d4b32b7c1e70833fdb
Abdulaziz Alothaim <A-Alothaim@Abdulaziz-Mac.local>
2016-10-04 10:19:12 +0100

    initial commit

---
+++ app/models/genre.rb
@@ -0,0 +1,8 @@
+class Genre < ActiveRecord::Base
+	has_many :movies, dependent:  :destroy
+	
+	# validation in rails helper
+	validates :name, presence: true
+	validates :name, uniqueness: true
+	
+end
